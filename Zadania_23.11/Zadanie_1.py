fd77c5b2123b2cb89e41e8b47d141ade16546c27
TheCiosiek <ciosiek@outlook.com>
2021-11-23 17:55:11 +0100

    comit

---
+++ Zadania_23.11/Zadanie_1.py
@@ -0,0 +1,11 @@
+import random
+def digitize(number):
+    str(number)
+    tab=[]
+    for i in number:
+        tab.append(i)
+    print (tab)
+
+number=input("Wprowadź liczbę, którą chcesz wprowadzić: ")
+digitize(number)
+

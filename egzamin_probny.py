da79cea508b9a66af4be7fe717c43890dfa74e2b
TheCiosiek <ciosiek@outlook.com>
2021-12-06 21:59:16 +0100

    dodanie .py

--- egzamin_probny.py
+++ egzamin_probny.py
@@ -1,41 +1,28 @@
-class animal():
+class Person():
     def __init__(self,name):
         self.name=name
     def get_name():
         pass
     def set_name():
         pass
-class Mammal(animal):
-    def __init_(self,name, no_legs,has_fur):
-        self.no_leg=no_leg
-        self.has_fur=has_fur
-    def get_no_legs():
+class Employee(Person):
+    def __init_(self,name, company,salary):
+        self.salary=salary
+        self.company=company
+    def get_salary():
         pass
-    def set_no_legs():
+    def set_salary():
         pass
-    def get_has_fur():
+class Manager(Employee):
+    def __init_(self,name, company,salary):
         pass
-    def set_has_fur():
+    def manage():
         pass
-class Canine(Mammal):
-    def __init_(self,name, no_legs,has_fur):
+class Associate(Employee):
+    def __init_(self,name, company,salary):
         pass
-    def howl():
-        pass
-    def bark():
-        pass
-class dog(Canine):
-    def __init_(self,name, no_legs,has_fur,kind,owner):
-        self.owner=owner
-        self.kind=kind
-    def get_kind():
-        pass
-    def set_kind():
-        pass
-    def get_owner():
-        pass
-    def set_owner():
-        pass        
+    def support():
+        pass     
 #------------------------------------------------------^ZADANIE 6^
 import math
 
@@ -45,7 +32,7 @@ def calc_circle_area(n):
 
 r=float(input("Wprowadź promień: "))
 area=calc_circle_area(r)
-print(area)
+print(f"{area}")
 #------------------------------------------------------
 def check_parity():
     number=input("wprowadź liczbę: ")
@@ -68,76 +55,75 @@ tab=input("Wprowadź liczby oddzielone spacją: ").split()
 tab=map(int,tab)
 element_present(n, tab)
 #------------------------------------------------------
-slownik = {}
 
 def print_menu():
-    print('1. Dodaj wpis')
-    print('2. Odzyskaj wpis')
-    print('3. Zmodyfikuj wpis')
-    print('4. Usu? wpis')
-    print('0. Wyjd?')
+    print("1. Dodaj nowy samochód")
+    print("2. Wyświetl samochód")
+    print("3. Zmodyfikuj samochód")
+    print("4. Usuń samochód")
+    print("0. Wyjdź z programu")
     
-def create_item(slownik):
+def add_car(cars):
     while True:
-        print("Podaj nowy klucz i wartość")
-        new_key=input("Nowy klucz: ")
+        new_key=input("Nowy samochód: ")
         try:
-            if new_key in slownik:
-                print("Klucz już istnieje, spróbuj jeszcze raz")
+            if new_key in cars:
+                print("Samochód już istnieje, spróbuj jeszcze raz")
             else:
                 raise TypeError
         except TypeError:
-            new_value=input("Nowa wartość: ")
-            slownik[new_key]=new_value
+            new_value=input("Cena: ")
+            cars[new_key]=new_value
             break
-    return slownik
-def read_item(slownik):
+    return cars
+def get_car(cars):
     while True:
         try:
-            find_key=input("Wprowadź klucz z którego chcesz odczytać wartość: ")
-            if find_key in slownik:
-                print(f"Wartość: {slownik[find_key]}")
+            find_key=input("Wprowadź samochód z którego cenę chcesz odczytać: ")
+            if find_key in cars:
+                print(f"Cena: {cars[find_key]}")
                 input("Wprowadź dowolny klawisz by kontynuować")
                 break
             else:
                 raise TypeError
         except TypeError:
-            print("Klucz nie znajduje się w słowniku, spróbuj jeszcze raz.")
-def update_item(slownik):
+            print("Samochód nie znajduje się w słowniku, spróbuj jeszcze raz.")
+def modify_car(cars):
     while True:
         try:
-            find_key=input("Wprowadź klucz, którego wartość chcesz zmienić: ")
-            if find_key in slownik:
-                new_value=input("Wprowadź nową wartość: ")
-                slownik[find_key]=new_value
+            find_key=input("Wprowadź samochód, którego cene chcesz zmienić: ")
+            if find_key in cars:
+                new_value=input("Wprowadź nową cenę: ")
+                cars[find_key]=new_value
                 break
             else:
                 raise TypeError
         except TypeError:
-            print("Klucz nie znajduje się w słowniku, spróbuj jeszcze raz.")
-    return slownik
-def delete_item(slownik):
+            print("Samochód nie znajduje się w słowniku, spróbuj jeszcze raz.")
+    return cars
+def delete_car(cars):
     while True:
         try:
-            find_key=input("Wprowadź klucz, który chcesz usunąć: ")
-            if find_key in slownik:
-                del slownik[find_key]
+            find_key=input("Wprowadź samochód, który chcesz usunąć: ")
+            if find_key in cars:
+                del cars[find_key]
                 break
             else:
                 raise TypeError
         except TypeError:
-            print("Klucz nie znajduje się w słowniku, spróbuj jeszcze raz.")
-    return slownik
+            print("Samochód nie znajduje się w słowniku, spróbuj jeszcze raz.")
+    return cars
+cars = {}
 while True:
     print_menu()
-    option = input("Podaj opcj?: ")
+    option = input("Podaj opcję: ")
     if option == '1':
-        slownik=create_item(slownik)
+        cars=add_car(cars)
     elif option == '2':
-        read_item(slownik)
+        get_car(cars)
     elif option == '3':
-        slownik=update_item(slownik)
+        cars=modify_car(cars)
     elif option == '4':
-        slownik=delete_item(slownik)
+        cars=delete_car(cars)
     elif option == '0':
         break

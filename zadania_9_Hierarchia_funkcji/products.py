24f0d172d4654a80d9b4bdce04bd51532dc4195d
TheCiosiek <ciosiek@outlook.com>
2022-01-24 15:49:17 +0100

    zadanko 9

--- zadania_9_Hierarchia_funkcji/products.py
+++ zadania_9_Hierarchia_funkcji/products.py
@@ -1,14 +1,39 @@
 def menu():
     dt.load_products()
     while dt.auth[0]==True:
-        option = options()
+        #option = options()
+        #TODO
         if option == 1:
-            change_product()
+            #add_product()
+            #TODO
+            pass
         elif option == 2:
-            add_product()
+            #del_product()
+            #TODO
+            pass
+        elif option == 3:
+            #change_product()
+            #TODO
+            pass
         elif option == 4:
             return
         elif option == 6:
             dt.auth=False, "user"
         elif option == 7:
             dt.auth = None, "user"
+
+def options():
+    #TODO
+    pass
+
+def add_product():
+    #TODO
+    pass
+
+def del_product():
+    #TODO
+    pass
+
+def change_product():
+    #TODO
+    pass
